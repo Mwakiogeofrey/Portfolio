aa7caa98032f576804593449b4bb75508a00740f
Mwakiogeofrey <mwakiogeofrey579@gmail.com>
2024-06-24 23:42:25 +0300

    modifing contact form

--- app.js
+++ app.js
@@ -95,7 +95,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }).then(response => {
             if (response.ok) {
                 form.reset();
-                alert(`Thank you for your message: ${message}`);
+                alert(`Thank you for your message`);
             } else {
                 response.json().then(data => {
                     if (Object.hasOwnProperty.call(data, 'errors')) {
